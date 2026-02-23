8519f2aa706bb257fef336a964e9a672d7b3bfba
GerardoCendejas <gerardocendejasmendoza@gmail.com>
2026-02-23 13:48:07 -0500

    feat: Time graph and hour

--- org-reading-stats.el
+++ org-reading-stats.el
@@ -5,7 +5,7 @@
   "Path to your org reading file.")
 
 (defun org-reading-stats-generate-json ()
-  "Process the file using simple string matching instead of complex regex."
+  "Simplified processing: look for @cite and YYYY-MM-DD independently."
   (interactive)
   (let* ((base (file-name-directory (or load-file-name (buffer-file-name) default-directory)))
          (web-dir (expand-file-name "web/" base))
@@ -20,17 +20,18 @@
       (goto-char (point-min))
       (while (not (eobp))
         (let ((line (buffer-substring-no-properties (line-beginning-position) (line-end-position))))
-          (cond
-           ;; 1. Look for the citation key
-           ((string-match "cite:@\\([^]]+\\)" line)
+          
+          ;; 1. Find citation key (starts with @, ends with ] or space)
+          (when (string-match "@\\([^] ]+\\)" line)
             (setq current-cite (match-string 1 line)))
-
-           ;; 2. Look for a date (YYYY-MM-DD) only if we have a pending citation
-           ((and current-cite (string-match "\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\)" line))
+          
+          ;; 2. Find date (any sequence XXXX-XX-XX)
+          (when (and current-cite (string-match "\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}[^]>]*\\)" line))
             (push `((cite . ,current-cite)
                     (timestamp . ,(match-string 1 line)))
                   results)
-            (setq current-cite nil))))
+            ;; Reset current-cite to wait for the next paper
+            (setq current-cite nil)))
         (forward-line 1)))
 
     (with-temp-file (expand-file-name "data.json" web-dir)
