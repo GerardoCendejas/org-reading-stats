2927a3cf5b032594ced114d9dde03837963a30fd
GerardoCendejas <gerardocendejasmendoza@gmail.com>
2026-02-23 16:07:39 -0500

    feat: Add org-roam note, and word count.

--- org-reading-stats.el
+++ org-reading-stats.el
@@ -1,15 +1,26 @@
 (require 'simple-httpd)
 (require 'json)
 
-;; ADJUST THIS: Point this to your actual org-roam notes folder
-(defvar org-roam-directory-path "~/.emacs.d/org/roam/" 
+;; ADJUST THIS: Path to your org-roam notes
+(defvar org-roam-directory-path "~/org-roam/" 
   "Path to your org-roam notes directory.")
 
 (defvar org-reading-stats-file "~/.emacs.d/org/read_papers.org"
   "Path to your org reading file.")
 
+(defun org-reading-stats-get-file-info (path)
+  "Open the file briefly to count words and links."
+  (with-temp-buffer
+    (insert-file-contents path)
+    (let ((word-count (count-words (point-min) (point-max)))
+          (link-count 0))
+      (goto-char (point-min))
+      (while (re-search-forward "\\[\\[" nil t)
+        (setq link-count (1+ link-count)))
+      (list word-count link-count))))
+
 (defun org-reading-stats-generate-json ()
-  "Process papers and check for note existence by looking for physical files."
+  "Process papers and extract depth info (words and links) from notes."
   (interactive)
   (let* ((script-dir (file-name-directory (or load-file-name (buffer-file-name) default-directory)))
          (web-dir (expand-file-name "web/" script-dir))
@@ -21,36 +32,40 @@
       (insert-file-contents org-reading-stats-file)
       (goto-char (point-min))
       (while (re-search-forward "@\\([^] \n\t]+\\)" nil t)
-        (let* ((cite-id (match-string 1)) ;; e.g., "enbodyCommunitywide..."
+        (let* ((cite-id (match-string 1))
                (timestamp "")
                (has-note-val "no")
+               (word-count 0)
+               (link-count 0)
                (search-limit (save-excursion (forward-line 4) (point))))
           
-          ;; 1. Capture timestamp
           (save-excursion
             (when (re-search-forward "\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}[^]>]*\\)" search-limit t)
               (setq timestamp (match-string 1))))
           
-          ;; 2. NEW METHOD: Check if any file in org-roam directory contains the ID in its name
-          ;; This is much more reliable than database queries if the DB is acting up.
           (let ((matching-files (directory-files-recursively 
                                  org-roam-directory-path 
                                  (regexp-quote cite-id))))
             (when matching-files
-              (setq has-note-val "yes")))
+              (let ((info (org-reading-stats-get-file-info (car matching-files))))
+                (setq has-note-val "yes")
+                (setq word-count (car info))
+                (setq link-count (cadr info)))))
           
           (push (list (cons 'cite (concat "@" cite-id))
                       (cons 'timestamp timestamp)
-                      (cons 'has_note has-note-val))
+                      (cons 'has_note has-note-val)
+                      (cons 'words word-count)
+                      (cons 'links link-count))
                 results)))
       
       (with-temp-file (expand-file-name "data.json" web-dir)
         (let ((json-encoding-pretty-print t))
           (insert (json-encode (reverse results)))))
       
-      (message "Dashboard updated: %d papers. Found files for: %d" 
+      (message "Dashboard updated: %d papers. Note Depth: %d total words." 
                (length results)
-               (length (seq-filter (lambda (x) (string= (cdr (assoc 'has_note x)) "yes")) results))))))
+               (apply '+ (mapcar (lambda (x) (cdr (assoc 'words x))) results))))))
 
 (defun org-reading-stats-start ()
   "Start the server."
